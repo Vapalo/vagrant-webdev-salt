99b4fd09de4b985737e1ba2892b42d89c8b54e03
Toni Vapalo <toni.vapalo@gmail.com>
2021-05-19 12:30:41 +0300

    Added react state to master install copy script

--- master-install.sh
+++ master-install.sh
@@ -5,6 +5,6 @@ echo "Installing salt master, creating /srv/salt and copying states to it"
 apt-get update > /dev/null
 apt-get install -y salt-master > /dev/null
 mkdir -p /srv/salt
-cp -nr vbox/ vagpy/ /srv/salt
+cp -nr vbox/ vagpy/ react/ /srv/salt
 
 echo "Done!"
